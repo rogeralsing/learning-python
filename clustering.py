f552eda0f00c616cd9d2fc32a79bd579b4252785
rogerjohansson <rogeralsing@gmail.com>
2020-12-12 21:47:53 +0100

    typings, set based cluster

--- clustering.py
+++ clustering.py
@@ -10,6 +10,9 @@ class ClusterPoint:
             children = []
         self.children = children
 
+    def add_child(self, child):
+        self.children.append(child)
+
 
 class Cluster:
     def __init__(self):
@@ -18,32 +21,34 @@ class Cluster:
     def xy(self):
         return [(cp.x, cp.y) for cp in self.skeleton]
 
-    def add(self, cp: ClusterPoint):
+    def add_point(self, cp: ClusterPoint):
         self.skeleton.append(cp)
 
 
 def cluster(points, max_distance=100):
-    ps = [ClusterPoint(x, y) for (x, y) in points]
+    ps: Set[ClusterPoint] = {ClusterPoint(x, y) for (x, y) in points}
 
-    def dist(p1: ClusterPoint, p2: ClusterPoint):
+    def dist(p1: ClusterPoint, p2: ClusterPoint) -> float:
         return hypot(p1.x - p2.x, p1.y - p2.y)
 
-    def recurse(current_point: ClusterPoint, bag: List[ClusterPoint], current_cluster: Cluster):
-        if current_point not in bag:
-            return
-
-        bag.remove(current_point)
-        current_cluster.add(current_point)
+    def recurse(current_point: ClusterPoint, bag: Set[ClusterPoint], current_cluster: Cluster):
+        current_cluster.add_point(current_point)
 
         children = list(filter(lambda p2: dist(current_point, p2) < max_distance, bag))
+
         for child_point in children:
-            current_point.children.append(child_point)
+            if child_point not in bag:
+                continue
+
+            bag.remove(child_point)
+
+            current_point.add_child(child_point)
             recurse(child_point, bag, current_cluster)
 
     clusters: List[Cluster] = []
 
     while len(ps) > 0:
-        p = ps[0]
+        p = ps.pop()
         c = Cluster()
         clusters.append(c)
         recurse(p, ps, c)
