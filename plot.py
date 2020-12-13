3dbc3af1d7796274bffd6291d0486a6b6744e1dd
rogerjohansson <rogeralsing@gmail.com>
2020-12-13 11:08:02 +0100

    use patches to render

--- plot.py
+++ plot.py
@@ -2,17 +2,6 @@ import matplotlib.colors as colors
 from descartes import PolygonPatch
 
 
-def plot_poly(plt, poly, color):
-    if poly.geom_type == 'MultiPolygon':
-        for g in poly.geoms:
-            plot_poly(plt, g, color)
-    elif poly.geom_type == 'Polygon' and not poly.is_empty:
-        plt.plot(*poly.exterior.xy, color=color)
-        plt.fill(*poly.exterior.xy, alpha=0.2, color=color)
-    else:
-        pass
-
-
 def plot(clusters, f, ax):
     palette = [color for name, color in colors.TABLEAU_COLORS.items()]
     for i, c in enumerate(clusters):
@@ -21,11 +10,12 @@ def plot(clusters, f, ax):
         points = c.xy()
         polygon = f(points)
 
-        #patch = PolygonPatch(polygon.buffer(0))
-        #ax.add_patch(patch)
-        plot_poly(ax, polygon, color)
-        plot_skeleton(ax, c, color)
-        plot_points(ax, color, points)
+        if not polygon.is_empty:
+            patch = PolygonPatch(polygon.buffer(0),color=color, alpha=0.2)
+            ax.add_patch(patch)
+
+            plot_skeleton(ax, c, color)
+            plot_points(ax, color, points)
 
 
 def plot_points(plt, color, points):
