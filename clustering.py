86982a0c97ae17e9dabd7a0b408e24b4f0e72f95
rogerjohansson <rogeralsing@gmail.com>
2020-12-13 20:37:48 +0100

    simplify code

--- clustering.py
+++ clustering.py
@@ -7,22 +7,16 @@ class ClusterPoint:
         self.y = y
         self.linked = set()
 
-    def unlink(self):
-        for child in self.linked:
-            child.linked.remove(self)
-        self.linked.clear()
-
-    def __len__(self):
-        return len(self.linked)
-
 
 def cluster(points, max_distance: float = 100):
     cluster_points = [ClusterPoint(*p) for p in points]
+    remaining_points = set(cluster_points)
+
+    # for every point, get every other point within range
+    # build ClusterPoints from the resulting data
     tree = KDTree(points)
     for i, l in enumerate(tree.query_ball_point(points, max_distance)):
         cluster_points[i].linked = {cluster_points[x] for x in l if x != i}
-
-    remaining_points = set(cluster_points)
 
     def recurse(current_point: ClusterPoint, current_cluster):
         if current_point not in remaining_points:
@@ -31,8 +25,8 @@ def cluster(points, max_distance: float = 100):
         current_cluster.append(current_point)
         remaining_points.remove(current_point)
 
-        for other_point in current_point.linked:
-            recurse(other_point, current_cluster)
+        for linked_point in current_point.linked:
+            recurse(linked_point, current_cluster)
 
         return current_cluster
 
