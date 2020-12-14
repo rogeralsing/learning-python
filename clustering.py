63c157d71761cc6e02df357ee07fc68f0b6eb2dd
rogerjohansson <rogeralsing@gmail.com>
2020-12-14 14:56:25 +0100

    dbscan

--- clustering.py
+++ clustering.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.spatial import KDTree
+from scipy.spatial import cKDTree
 from sklearn.cluster import DBSCAN
 
 
@@ -31,17 +31,17 @@ def sk_dbscan(data, max_distance: float = 100, min_pts: int = 3):
     clustering = DBSCAN(eps=max_distance, min_samples=min_pts).fit(np.array(data))
     clusters = [list() for _ in range(max(clustering.labels_) + 1)]
 
-    for i, p in enumerate(data):
+    for i, p in enumerate(points):
         if clustering.labels_[i] != -1:
             c = clusters[clustering.labels_[i]]
-            c += [points[i]]
+            c += [p]
 
     return clusters
 
 
 def get_cluster_points(data, max_distance):
     points = [ClusterPoint(p) for p in data]
-    tree = KDTree(data)
+    tree = cKDTree(data)
     for i, l in enumerate(tree.query_ball_point(data, max_distance)):
         points[i].neighbours = [points[x] for x in l if x != i]
     return points
