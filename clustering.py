f4cd75559dc09b774547cb97db70899382089bb8
rogerjohansson <rogeralsing@gmail.com>
2020-12-14 13:22:35 +0100

    dbscan

--- clustering.py
+++ clustering.py
@@ -12,19 +12,17 @@ class ClusterPoint:
         return self
 
 
-def dbscan(points, max_distance: float = 100, min_pts=3):
-    cluster_points = [ClusterPoint(p) for p in points]
+def dbscan(data, max_distance: float = 100, min_pts=3):
+    points = [ClusterPoint(p) for p in data]
 
-    # for every point, get every other point within range
-    # build ClusterPoints from the resulting data
-    tree = KDTree(points)
-    for i, l in enumerate(tree.query_ball_point(points, max_distance)):
-        cluster_points[i].neighbours = {cluster_points[x] for x in l if x != i}
+    tree = KDTree(data)
+    for i, l in enumerate(tree.query_ball_point(data, max_distance)):
+        points[i].neighbours = [points[x] for x in l if x != i]
 
     def scan(cluster):
         for p in cluster:
-            cluster.extend([n.take() for n in p.neighbours if not n.taken])
+            cluster += [n.take() for n in p.neighbours if not n.taken]
 
         return cluster
 
-    return [scan([p.take()]) for p in cluster_points if not p.taken and len(p.neighbours) >= min_pts]
+    return [scan([p.take()]) for p in points if not p.taken and len(p.neighbours) >= min_pts]
