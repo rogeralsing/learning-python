6bf32ec19999f31b9143dab732977cc9858329cc
roger johansson <roger.johansson@abax.com>
2021-05-27 20:29:42 +0200

    done...

--- curve.py
+++ curve.py
@@ -10,29 +10,17 @@ def calc_point(p1, p2, step, steps=10):
     return x, y
 
 
-def calc_line(p1, p2, steps=10):
-    return [calc_point(p1, p2, i, steps) for i in range(0, steps + 1)]
-
-
 def curve(p1, p2, p3, p4, steps=10):
-    left = calc_line(p1, p2, steps)
-    right = calc_line(p4, p3, steps)
-    top = calc_line(p2, p4, steps)
-
     plt.plot(*zip(*[p1, p2, p4, p3]))
 
     points = [calc_point(
-        calc_point(left[i], top[i], i, steps),
-        calc_point(top[i], right[i], i, steps),
+        calc_point(calc_point(p1, p2, i, steps), calc_point(p2, p4, i, steps), i, steps),
+        calc_point(calc_point(p2, p4, i, steps), calc_point(p4, p3, i, steps), i, steps),
         i, steps) for i in range(0, steps + 1)]
 
     plt.plot(*zip(*points))
 
 
-curve((5, 20),
-      (0, 30),
-      (15, 20),
-      (25, 40),
-      20)
+curve((5, 20), (0, 30), (15, 20), (25, 40), 20)
 
 plt.show()
