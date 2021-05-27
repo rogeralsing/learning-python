a37cee582590f9b0a4dac0ef38a6c99e65fa5b8f
roger johansson <roger.johansson@abax.com>
2021-05-27 20:04:03 +0200

    Bézier curve

--- curve.py
+++ curve.py
@@ -17,33 +17,41 @@ def calc_line(x1, y1, x2, y2, steps=10):
     return res
 
 
-def curve(x1, y1, x2, y2, x3, y3, x4, y4):
-    left = calc_line(x1, y1, x2, y2)
-    right = calc_line(x3, y3, x4, y4)
-    top = calc_line(x2, y2, x4, y4)
+def calc_point(x1, y1, x2, y2, step, steps=10):
+    dx = (x2 - x1) / steps
+    dy = (y2 - y1) / steps
+
+    x = x1 + dx * step
+    y = y1 + dy * step
+
+    return x, y
+
+
+def curve(x1, y1, x2, y2, x3, y3, x4, y4, steps=10):
+    left = calc_line(x1, y1, x2, y2, steps)
+    right = calc_line(x3, y3, x4, y4, steps)
+    top = calc_line(x2, y2, x4, y4, steps)
 
     plt.plot(*zip(*[(x1, y1), (x2, y2), (x4, y4), (x3, y3)]))
 
     points = []
-    for i in range(0, 10 + 1):
+    for i in range(0, steps + 1):
         p_left = left[i]
         p_top = top[i]
-        p_right = right[10 - i]
+        p_right = right[steps - i]
 
-        l1 = calc_line(p_left[0], p_left[1], p_top[0], p_top[1])
-        p1 = l1[i]
-        l2 = calc_line(p_top[0], p_top[1], p_right[0], p_right[1])
-        p2 = l2[i]
-        l3 = calc_line(p1[0], p1[1], p2[0], p2[1])
-        p = l3[i]
+        p1 = calc_point(p_left[0], p_left[1], p_top[0], p_top[1], i, steps)
+        p2 = calc_point(p_top[0], p_top[1], p_right[0], p_right[1], i, steps)
+        p_final = calc_point(p1[0], p1[1], p2[0], p2[1], i, steps)
 
-        points.append(p)
+        points.append(p_final)
     plt.plot(*zip(*points))
 
 
 curve(5, 20,
       0, 30,
       15, 20,
-      25, 40)
+      25, 40,
+      20)
 
 plt.show()
