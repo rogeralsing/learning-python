9e3804518fbaaadbbf991a5fba8f52eef2da21cd
roger johansson <roger.johansson@abax.com>
2021-05-27 20:22:29 +0200

    done...

--- curve.py
+++ curve.py
@@ -2,16 +2,16 @@
 import matplotlib.pyplot as plt
 
 
-def calc_line(p1, p2, steps=10):
-    return [calc_point(p1, p2, i, steps) for i in range(0, steps + 1)]
-
-
 def calc_point(p1, p2, step, steps=10):
     (x1, y1), (x2, y2) = p1, p2
     x = x1 + (x2 - x1) / steps * step
     y = y1 + (y2 - y1) / steps * step
 
     return x, y
+
+
+def calc_line(p1, p2, steps=10):
+    return [calc_point(p1, p2, i, steps) for i in range(0, steps + 1)]
 
 
 def curve(p1, p2, p3, p4, steps=10):
@@ -21,14 +21,11 @@ def curve(p1, p2, p3, p4, steps=10):
 
     plt.plot(*zip(*[p1, p2, p4, p3]))
 
-    points = []
-    for i in range(0, steps + 1):
-        p_final = calc_point(
-            calc_point(left[i], top[i], i, steps),
-            calc_point(top[i], right[i], i, steps),
-            i, steps)
+    points = [calc_point(
+        calc_point(left[i], top[i], i, steps),
+        calc_point(top[i], right[i], i, steps),
+        i, steps) for i in range(0, steps + 1)]
 
-        points.append(p_final)
     plt.plot(*zip(*points))
 
 
